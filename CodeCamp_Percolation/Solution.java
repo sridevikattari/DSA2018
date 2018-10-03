aacf9af3ee803da98a9545e241751b24c6cd1a83
sridevi.kattari@gmail.com <sridevi.kattari@gmail.com>
2018-10-03 10:13:27 +0530

    "check_style_score -> 8 of 24 passed. style score: 0/1 "

--- CodeCamp_Percolation/Solution.java
+++ CodeCamp_Percolation/Solution.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Solution 
 {
 
-public static void mrn(String[] args) 
+public static void main(String[] args) 
 {
 	// TODO Auto -  generated method stub
 	Scanner scan = new Scanner(System.in);
